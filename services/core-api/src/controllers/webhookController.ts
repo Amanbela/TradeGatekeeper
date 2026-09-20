c715b5ab7cbc5ae9cccac3b9bd803bb54f4868c4
Amanbela <amanbela8827@gmail.com>
2026-09-20 18:45:11 +0530

    Fix UUID import in webhook controller

--- services/core-api/src/controllers/webhookController.ts
+++ services/core-api/src/controllers/webhookController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { v4 as uuidv4 } from 'crypto';
+import { v4 as uuidv4 } from 'uuid';
 import { SignalPayload, MlFeatureVector } from '../types';
 import { RMSService } from '../services/rmsService';
 import { getCandleManager } from '../engine/candleManager';
